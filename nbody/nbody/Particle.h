08c2c3f33edb42235bab893f367708fa21a512ac
migiesmith <migiesmith@gmail.com>
2016-11-15 21:16:18 +0000

    Grid approach

---
+++ nbody/nbody/Particle.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <glm\glm.hpp>
+
+struct Particle {
+	glm::vec3 pos;
+	float mass;
+};
--- nbody/nbody/main.cpp
+++ nbody/nbody/main.cpp
@@ -1,5 +1,11 @@
 // nbody.cpp : Defines the entry point for the console application.
-//
+
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "freeglut.lib")
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "FreeImage.lib")
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
@@ -7,10 +13,11 @@
 #include <map>
 #include <sstream>
 
-
 #include "Renderer.h"
 #include "Shader.h"
 #include "Util.h"
+
+#include "Node.h"
 
 
 using namespace std;
@@ -21,8 +28,17 @@ using namespace Util;
 map<string, unsigned int> textures;
 map<string, Shader*> shaders;
 
+Node root;
+vector<Particle*> particles;
+
+
+
 // http://www.browndeertechnology.com/docs/BDT_OpenCL_Tutorial_NBody-rev3.html#opencl
 // https://github.com/larsendt/opencl-nbody
+
+/* Barnes-Hut */
+// http://codereview.stackexchange.com/questions/95932/barnes-hut-n-body-simulator
+// http://www.andrew.cmu.edu/user/esp/
 
 void init() {
 	Shader* colourShader = new Shader();
@@ -32,7 +48,25 @@ void init() {
 	shaders["colourPass"] = colourShader;
 }
 
+void calcForce() {
+	for (auto p : particles) {
+		vec3 force = root.calcForceFromTree(p);
+		p->pos += force;
+		/*
+		p->pos.x = max(min(p->pos.x, root.getWidth()), 0.0f);
+		p->pos.y = max(min(p->pos.y, root.getHeight()), 0.0f);
+		*/
+	}
+}
+
 void update(float deltaTime) {
+	root.deleteTree();
+
+	for (auto p : particles) {
+		root.insertParticle(p);
+	}
+
+	calcForce();
 
 }
 
@@ -41,23 +75,38 @@ void render() {
 	Renderer::getInstance().bindShader(shaders["colourPass"]);
 
 	// Set the MVP matrix
-	glUniformMatrix4fv(Renderer::getInstance().getActiveShader()->getUniformLoc("MVP"), 1, false, value_ptr(mat4()));
+	Shader* currentShader = Renderer::getInstance().getActiveShader();
 
-	glBegin(GL_TRIANGLES);
-	glVertex3f(-1,0,0);
-	glVertex3f(0, 2, 0);
-	glVertex3f(1, 0, 0);
-	glEnd();
+	float ratio = (float)Renderer::getWidth() / (float)Renderer::getHeight();
+	float w = 100.0f;
+	float h = 100.0f * (1.0f / ratio);
+	mat4 MVP = glm::translate(glm::ortho(-w,w,h,-h, 0.5f, 1000.0f), vec3(-50.0f, -50.0f, -100.0f));
+	//mat4 MVP = glm::translate(glm::perspective(90.0f, ratio, 0.5f, 5000.0f), vec3(0,0,-100.0f));
 
+	glUniformMatrix4fv(currentShader->getUniformLoc("MVP"), 1, false, value_ptr(MVP));
+	glUniform4f(currentShader->getUniformLoc("colour"), 1,0,0,1);
+
+	glDisable(GL_CULL_FACE);
+
+	root.renderGrid();
+
+	glUniform4f(currentShader->getUniformLoc("colour"), 0, 1, 0, 1);
+	glPointSize(2.0f);
+	for (auto p : particles) {
+		glBegin(GL_POINTS);
+		glVertex3f(p->pos.x, p->pos.y, p->pos.z);
+		glEnd();
+	}
 
 }
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-	/*
+
+	
 	if (key == GLFW_KEY_F1 && action == GLFW_PRESS) {
-		renderSSAO = !renderSSAO;
+		root.deleteTree();
 	}
-	else if (key == GLFW_KEY_F2 && action == GLFW_PRESS) {
+	/*else if (key == GLFW_KEY_F2 && action == GLFW_PRESS) {
 		renderLensFlare = !renderLensFlare;
 	}
 
@@ -70,6 +119,25 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 
 int main()
 {
+	float width = 100.0, height = 100.0f;
+	float sqrtNodeCount = 16;
+
+	root = Node(0, 0, width, height);
+
+	for (unsigned int yy = 0; yy < sqrtNodeCount; yy++) {
+		for (unsigned int xx = 0; xx < sqrtNodeCount; xx++) {
+			Particle* p = new Particle();
+			p->pos = vec3(xx * width / sqrtNodeCount + (width / sqrtNodeCount) / 2, yy * height / sqrtNodeCount + (height / sqrtNodeCount) / 2, 0);
+			p->mass = 100000;
+			particles.push_back(p);
+		}
+	}
+
+	Particle* p2 = new Particle();
+	p2->pos = vec3(55,55,0);
+	p2->mass = 1;
+	particles.push_back(p2);
+
 	// Set the functions for the renderer to call
 	Renderer::getInstance().setInit(init);
 	Renderer::getInstance().setUpdate(update);
