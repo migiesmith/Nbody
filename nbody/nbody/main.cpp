1e57c5db71ba72229025f19396d4178f16efbc80
migiesmith <migiesmith@gmail.com>
2016-11-16 18:13:04 +0000

    Improved brute force method (also removed mass as it is assumed each has a mass of 1)

--- nbody/nbody/main.cpp
+++ nbody/nbody/main.cpp
@@ -13,14 +13,13 @@
 #include <map>
 #include <sstream>
 #include <chrono>
+#include <random>
 
 #include <omp.h>
 
 #include "Renderer.h"
 #include "Shader.h"
 #include "Util.h"
-
-#include "Particle.h"
 
 
 using namespace std;
@@ -32,21 +31,21 @@ using namespace Util;
 map<string, unsigned int> textures;
 map<string, Shader*> shaders;
 
+struct Particle {
+	vec3 pos;
+	vec3 velocity;
+};
+
 vector<Particle*> particles;
 
 const float GRAVITY = 6.6742E-11;
 
-long lastFrame;
-float deltaTime;
 
-const float PHYSICS_TIME = 0.001f;
+const float PHYSICS_TIME = 10.0f;
+const float DAMPENING = 0.99f;
+const float SIM_WIDTH = 1000.0f;
+const float SIM_HEIGHT = 1000.0f;
 
-// http://www.browndeertechnology.com/docs/BDT_OpenCL_Tutorial_NBody-rev3.html#opencl
-// https://github.com/larsendt/opencl-nbody
-
-/* Barnes-Hut */
-// http://codereview.stackexchange.com/questions/95932/barnes-hut-n-body-simulator
-// http://www.andrew.cmu.edu/user/esp/
 
 void init() {
 	Shader* colourShader = new Shader();
@@ -57,16 +56,25 @@ void init() {
 }
 
 void calcForce() {
-//#pragma omp parallel for num_threads(6)
+	float EPS = 3E4;
+#pragma omp parallel for num_threads(6)
 	for (int i = 0; i < particles.size(); i++) {
-		for (int j = i+1; j < particles.size(); j++) {
-			particles[i]->addForce(*particles[j]);
+		vec3 vel = vec3(0.0f, 0.0f, 0.0f);
+		for (int j = 0; j < particles.size(); j++) {
+			if (i == j)
+				continue;
+
+			vec3 distVec = particles[j]->pos - particles[i]->pos;
+			float dist = dot(distVec, distVec) + EPS;
+			if (dist > 0.1f) {
+				vel += distVec * pow(1.0f / sqrtf(dist), 3);
+			}
 		}
-	}
-	//Then, loop again and update the bodies using timestep dt
-//#pragma omp parallel for num_threads(6)
-	for (int i = 0; i < particles.size(); i++) {
-		particles[i]->update(PHYSICS_TIME);
+		particles[i]->velocity += PHYSICS_TIME * vel * DAMPENING;
+		particles[i]->pos += particles[i]->velocity;
+
+		particles[i]->pos.x = min(max(particles[i]->pos.x, -SIM_WIDTH / 2.0f), SIM_WIDTH / 2.0f);
+		particles[i]->pos.y = min(max(particles[i]->pos.y, -SIM_HEIGHT / 2.0f), SIM_HEIGHT / 2.0f);
 	}
 }
 
@@ -82,24 +90,25 @@ void render() {
 	Shader* currentShader = Renderer::getInstance().getActiveShader();
 
 	float ratio = (float)Renderer::getWidth() / (float)Renderer::getHeight();
-	float w = 100.0f;
-	float h = 100.0f * (1.0f / ratio);
-	mat4 MVP = glm::translate(glm::ortho(-w,w,h,-h, 0.5f, 1000.0f), vec3(-50.0f, -50.0f, -100.0f));
+	float w = SIM_WIDTH;
+	float h = SIM_HEIGHT * (1.0f / ratio);
+	mat4 MVP = glm::translate(glm::ortho(-w,w,h,-h, 0.5f, 1000.0f), vec3(0.0f, 0.0f, -100.0f));
 	//mat4 MVP = glm::translate(glm::perspective(90.0f, ratio, 0.5f, 5000.0f), vec3(0,0,-100.0f));
 
 	glUniformMatrix4fv(currentShader->getUniformLoc("MVP"), 1, false, value_ptr(MVP));
-	glUniform4f(currentShader->getUniformLoc("colour"), 1,0,0,1);
 
 	glDisable(GL_CULL_FACE);
 
-
-	glUniform4f(currentShader->getUniformLoc("colour"), 0, 1, 0, 1);
+	
 	glPointSize(2.0f);
 	for (auto p : particles) {
+		float speed = abs(p->velocity.x) * abs(p->velocity.y) + 0.2f;
+		glUniform4f(currentShader->getUniformLoc("colour"), speed, speed, speed, 1);
 		glBegin(GL_POINTS);
 		glVertex3f(p->pos.x, p->pos.y, p->pos.z);
 		glEnd();
 	}
+	
 }
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
@@ -117,35 +126,30 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 	*/
 }
 
-void createGridOfParticles(float startX, float startY, float width, float height, int nodes) {
-	int sqrtNodeCount = sqrt(nodes);
-	for (unsigned int yy = 0; yy < sqrtNodeCount; yy++) {
-		for (unsigned int xx = 0; xx < sqrtNodeCount; xx++) {
+void generateParticles(int particleCount) {
+	std::mt19937 rng;
+	rng.seed(std::random_device()());
+	std::uniform_int_distribution<std::mt19937::result_type> distW(0, SIM_WIDTH*0.9f);
+	std::uniform_int_distribution<std::mt19937::result_type> distH(0, SIM_HEIGHT*0.9f);
+
+	for (unsigned int i = 0; i < particleCount; i++) {
 			Particle* p = new Particle();
-			p->pos = vec3(startX + xx * width / sqrtNodeCount + (width / sqrtNodeCount) / 2, startY + yy * height / sqrtNodeCount + (height / sqrtNodeCount) / 2, 0);
-			p->mass = 1.98892e18;
+			p->pos = vec3(distW(rng) - SIM_WIDTH*0.5f, distH(rng) - SIM_HEIGHT*0.5f, 0);
 			particles.push_back(p);
-		}
 	}
 }
 
 int main()
 {
-	createGridOfParticles(0, 0, 100, 100, 16*16);
-
-	/*
-	createGridOfParticles(0, 0, 20, 20, 32);
-	createGridOfParticles(0, 0, 100, 100, 64);
-	*/
+	generateParticles(2048);
+	
 
 	// Set the functions for the renderer to call
 	Renderer::getInstance().setInit(init);
 	Renderer::getInstance().setUpdate(update);
 	Renderer::getInstance().setRender(render);
 	Renderer::getInstance().setKeyCallback(keyCallback);
-
-	lastFrame = duration_cast<nanoseconds>(system_clock::now().time_since_epoch()).count();
-
+	
 	// Start the renderer and return any error codes on completion
 	return Renderer::getInstance().start(1280, 720);
 	cin.get();
